34ee20437ce99743788cb10484854340dad78426
FebrianAditya <febrian.aditya@student.upi.edu>
2024-10-11 15:47:16 +0700

    update code

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+
+
+let cekHeader = document.querySelector("#header-details")
+let cekHeader2 = document.getElementById("header-details")
+let cekHeader3 = document.getElementsByClassName("details")
+// console.log(cekHeader3);
