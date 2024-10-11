bad12c55888872fe5516b56770fa6a349d6e5da1
FebrianAditya <febrian.aditya@student.upi.edu>
2024-10-11 16:04:46 +0700

    change text align

--- js/index.js
+++ js/index.js
@@ -4,4 +4,4 @@
 let cekHeader = document.querySelector("#header-details")
 let cekHeader2 = document.getElementById("header-details")
 let cekHeader3 = document.getElementsByClassName("details")
-// console.log(cekHeader3);
+console.log(cekHeader3);
