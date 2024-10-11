b2f21ecddd17418351e696a98da8cf83a0e9fb1f
FebrianAditya <febrian.aditya@student.upi.edu>
2024-10-11 16:23:08 +0700

    add file js

--- js/index.js
+++ js/index.js
@@ -4,4 +4,26 @@
 let cekHeader = document.querySelector("#header-details")
 let cekHeader2 = document.getElementById("header-details")
 let cekHeader3 = document.getElementsByClassName("details")
-console.log(cekHeader3);
+
+
+
+// let name = document.getElementById("name-user").value
+// console.log(cekHeader3);
+
+
+
+function handleSubmit(event) {
+    // console.log(event, "==> ini event");
+    
+    event.preventDefault()    // untuk matiiin kebiasaan dari submit
+    // console.log("aku terpanggil");
+    const valueNameUser = document.getElementById("name-user").value
+    console.log(valueNameUser);
+
+
+    const fullNameUser = document.getElementById("fullname-user").innerHTML = valueNameUser
+    // const roleUser = document.getElementById("role-user").innerHTML
+    console.log(fullNameUser);
+    
+    
+}
